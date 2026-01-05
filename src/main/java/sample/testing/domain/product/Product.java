132ecfdfc46cd2fccf5593fcd730b5bc41982dcf
dongwookkim <dongzzi101@gmail.com>
2026-01-05 12:41:51 +0900

    persistent layer test 1. active 설정 방법 2. size -> extracting -> contains

--- src/main/java/sample/testing/domain/product/Product.java
+++ src/main/java/sample/testing/domain/product/Product.java
@@ -2,6 +2,7 @@ package sample.testing.domain.product;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,4 +27,12 @@ public class Product {
 
     private int price;
 
+    @Builder
+    private Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
